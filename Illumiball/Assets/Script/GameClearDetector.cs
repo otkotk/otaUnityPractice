7425602bfdf74873d5d69871ea97ad00be708efa
otkotk <ser1ous.bo.36dm@gmail.com>
2020-06-17 16:29:27 +0900

    chapter4

---
+++ Illumiball/Assets/Script/GameClearDetector.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameClearDetector : MonoBehaviour
+{
+    public Hole holeRed;
+    public Hole holeBlue;
+    public Hole holeGreen;
+
+    void OnGUI()
+    {
+        GUI.matrix = Matrix4x4.Scale(Vector3.one * 2);
+        //すべてのボールが入ったらラベルを表示
+        if(holeRed.IsHolding() && holeBlue.IsHolding() && holeGreen.IsHolding())
+        {
+            GUI.Label(new Rect(44,88,100,30), "YHEAAAAA!!!");
+        }
+    }
+}
