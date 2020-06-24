e605b634941cac8788c445440117223a10e3ac06
otkotk <ser1ous.bo.36dm@gmail.com>
2020-06-24 15:18:37 +0900

    衝突したら、後ろに下がりたい^^;

---
+++ Neziko Run/Assets/Script/GameController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public NejikoController nejiko;
+    public Text scoreText;
+    public LifePanel lifePanel;
+
+    public void Update()
+    {
+        // スコアを更新
+        int score = CalcScore();
+        scoreText.text = "Score : " + score + "m";
+
+        // ライフパネルを更新
+        lifePanel.UpdateLife(nejiko.Life());
+    }
+
+    int CalcScore()
+    {
+        // ネジ子の走行距離をスコアとする
+        return (int)nejiko.transform.position.z;
+    }
+}
--- Neziko Run/Assets/Script/NejikoController.cs
+++ Neziko Run/Assets/Script/NejikoController.cs
@@ -7,18 +7,32 @@ public class NejikoController : MonoBehaviour
     const int MinLane = -2;
     const int MaxLane = 2;
     const float LaneWidth = 1.0f;
+    const int DefaultLife = 5;
+    const float StunDuration = 0.5f;
 
     CharacterController controller;
     Animator animator;
 
     Vector3 moveDirection = Vector3.zero;
     int targetLane;
+    int life = DefaultLife;
+    float recoverTime = 0.0f;
 
     public float gravity;
     public float speedZ;
     public float speedX;
     public float speedJump;
     public float accelerationZ;
+
+    public int Life()
+    {
+        return life;
+    }
+
+    bool IsStun()
+    {
+        return recoverTime > 0.0f || life <= 0;
+    }
 
     void Start()
     {
@@ -34,13 +48,23 @@ public class NejikoController : MonoBehaviour
         if (Input.GetKeyDown("right")) MoveToRight();
         if (Input.GetKeyDown("space")) Jump();
 
-        // 徐々に加速しZ方向に常に前進させる
-        float acceleratedZ = moveDirection.z + (accelerationZ * Time.deltaTime);
-        moveDirection.z = Mathf.Clamp(acceleratedZ, 0, speedZ);
+        if (IsStun())
+        {
+            // 動きを止め、気絶状態からの復帰カウントを進める
+            moveDirection.x = 0.0f;
+            moveDirection.z = 0.0f;
+            recoverTime -= Time.deltaTime;
+        }
+        else
+        {
+            // 徐々に加速しZ方向に常に前進させる
+            float acceleratedZ = moveDirection.z + (accelerationZ * Time.deltaTime);
+            moveDirection.z = Mathf.Clamp(acceleratedZ, 0, speedZ);
 
-        // X方向は目標のポジションまで差分の割合で速度を計算
-        float ratioX = (targetLane * LaneWidth - transform.position.x) / LaneWidth;
-        moveDirection.x = ratioX * speedX;
+            // X方向は目標のポジションまで差分の割合で速度を計算
+            float ratioX = (targetLane * LaneWidth - transform.position.x) / LaneWidth;
+            moveDirection.x = ratioX * speedX;
+        }
 
         // 重力分の力を毎フレーム追加
         moveDirection.y -= gravity * Time.deltaTime;
@@ -59,23 +83,48 @@ public class NejikoController : MonoBehaviour
     // 左のレーンに移動を開始
     public void MoveToLeft()
     {
+        if (IsStun()) return;
         if (controller.isGrounded && targetLane > MinLane) targetLane--;
     }
 
     // 右のレーンに移動を開始
     public void MoveToRight()
     {
+        if (IsStun()) return;
         if (controller.isGrounded && targetLane < MaxLane) targetLane++;
     }
 
     public void Jump()
     {
+        if (IsStun()) return;
         if (controller.isGrounded)
         {
             moveDirection.y = speedJump;
 
             // ジャンプトリガーを設定
             animator.SetTrigger("jump");
+        }
+    }
+
+    // CharacterControllerに衝突判定が生じたときの処理
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (IsStun()) return;
+        if(hit.gameObject.tag == "Robo")
+        {
+            // ライフを減らして気絶状態に移行
+            life--;
+            recoverTime = StunDuration;
+
+            // ダメージトリガーを設定
+            animator.SetTrigger("damage");
+
+            // (改良)少し後ろに下がる
+            float acceleratedZ = moveDirection.z - (accelerationZ * Time.deltaTime);
+            moveDirection.z = Mathf.Clamp(acceleratedZ, 0, speedZ);
+
+            // ヒットしたオブジェクトは削除
+            Destroy(hit.gameObject);
         }
     }
 }
